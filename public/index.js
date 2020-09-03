01574a5d0158f69dc4c004180213e4d553d313ea
Gelo Ramos <christopherangelo.ramos.iics@ust.edu.ph>
2020-09-03 12:29:33 +0800

    Revert "Revert "[feat][Gelo]""
    This reverts commit 99fbaa620deb883c82c1e134ae8d9b4a91a72b6c.

---
+++ public/index.js
@@ -0,0 +1,24 @@
+function reset() {
+  document.getElementById("myImage").src = "img/living-room.png.png";
+  document.getElementById("feat-type").innerHTML = "";
+  document.getElementById("feat-desc").innerHTML = "";
+}
+function feature1() {
+  document.getElementById("myImage").src = "img/sofa.png";
+  document.getElementById("feat-type").innerHTML = "Feature1";
+  document.getElementById("feat-desc").innerHTML =
+    "Feature1 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis quod consectetur repellat autem, cupiditate et accusamus ipsum. Aperiam minus itaque eveniet cum aliquid dolore, ab, non quia ullam beatae corrupti? ";
+}
+function feature2() {
+  document.getElementById("myImage").src = "img/feature2.png";
+  document.getElementById("feat-type").innerHTML = "Feature2";
+  document.getElementById("feat-desc").innerHTML =
+    "Feature2 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis quod consectetur repellat autem, cupiditate et accusamus ipsum. Aperiam minus itaque eveniet cum aliquid dolore, ab, non quia ullam beatae corrupti?";
+}
+
+function feature3() {
+  document.getElementById("myImage").src = "img/feature3.png";
+  document.getElementById("feat-type").innerHTML = "Feature3";
+  document.getElementById("feat-desc").innerHTML =
+    "Feature3 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis quod consectetur repellat autem, cupiditate et accusamus ipsum. Aperiam minus itaque eveniet cum aliquid dolore, ab, non quia ullam beatae corrupti?";
+}
