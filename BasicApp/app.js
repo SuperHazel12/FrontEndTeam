eb5f7ab01f4a4add617417c939e57e089710ea56
kylie777 <kyliejanechua@gmail.com>
2020-10-01 19:26:54 +0800

    [fix] [Kylie] fixed close button for avatar

--- BasicApp/app.js
+++ BasicApp/app.js
@@ -4,8 +4,6 @@ var modal = document.getElementById("myModal");
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
-var btnClose= document.getElementById("closeBtn");
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
@@ -14,7 +12,7 @@ btn.onclick = function () {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// When the user clicks on <span> (close), close the modal
 span.onclick = function () {
     modal.style.display = "none";
 }
@@ -23,5 +21,28 @@ span.onclick = function () {
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
+    }
+}
+
+
+//Avatar Modal
+
+var span1 = document.getElementsByClassName("close1")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+var modalAvatar = document.getElementById("avatarModal");
+// When the user clicks on <span> (close), close the modal
+span1.onclick = function () {
+    modalAvatar.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modalAvatar) {
+        modalAvatar.style.display = "none";
     }
 }
