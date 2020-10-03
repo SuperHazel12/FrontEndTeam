92e367f3ec5730be484bef6ca95a9fee0d16cc18
Gelo Ramos <christopherangelo.ramos.iics@ust.edu.ph>
2020-10-03 12:37:48 +0800

    [feat][Gelo] Fix chatbox

--- BasicApp/script.js
+++ BasicApp/script.js
@@ -1,3 +1,4 @@
+//chatbox
 (function () {
   $("#live-chat header").on("click", function () {
     $(".chat").slideToggle(300, "swing");
@@ -9,13 +10,21 @@
     $(".chat-message-counter").fadeToggle(300, "swing");
   });
 
-  $(".chat-close").on("click", function (e) {
-    e.preventDefault();
-    $("#live-chat").fadeOut(300);
+  // $(".chat-close").on("click", function (e) {
+  //   e.preventDefault();
+  //   $("#live-chat").fadeOut(300);
+  // });
+  $(document).ready(function () {
+    $(".chat-close").click(function () {
+      $("#live-chat").hide("slow");
+    });
+    $("#chatbox").click(function () {
+      $("#live-chat").show("slow");
+    });
   });
 })();
 
-// Javascript from W3school
+//dropdown
 function dropdownUser() {
   document.getElementById("userDropdown").classList.toggle("active");
 }
@@ -45,5 +54,32 @@ window.onclick = function (event) {
         openDropdown.classList.remove("show");
       }
     }
+  }
+};
+
+//js for modal
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
 };
